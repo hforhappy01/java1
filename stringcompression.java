8eb352d65f9706a524111ccb578d0f2b6f886e5c
hforhappy01 <63479065+hforhappy01@users.noreply.github.com>
2021-11-07 01:47:38 +0530

    Update stringcompression.java

--- stringcompression.java
+++ stringcompression.java
@@ -3,10 +3,15 @@ import java.util.*;
 
 public class stringcompression {
 
-	public static String compression1(String str){
-		// write your code here
+	public static String compression1(String str){ // wil give aabbcccccdddddddddddd in abcd form
+		//creating a string ans which will have the string values
         String ans = "";
         for(int i = 0; i<str.length(); i++){
+		// why I used while loop here?? it is a interesting question
+		/* I used while loop here to increament the value of i to that value after which the same letter not repeats
+		for ex aaabbcc - here we will move the i inndex to i- [2] so after that we can have left with only one{a} in our hand and we will print that value
+		for the same ex. i will be 2 str at 2 is -> a after which i becomes 3 and same things will happen*/
+	
             while(i<str.length()-1 && str.charAt(i)==str.charAt(i+1)){
                 i++;
             }
@@ -16,7 +21,7 @@ public class stringcompression {
 		return ans;
 	}
 
-	public static String compression2(String str){
+	public static String compression2(String str){ // will give aaabbc in a3b2c form
 		
 
 		 String ans = "";
